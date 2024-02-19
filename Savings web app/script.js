67d21a54e3acda0a3f6d5d6e6317db5eed049114
Amine Sab <156006449+kokoga12@users.noreply.github.com>
2024-02-19 12:50:50 +0100

    Delete Savings web app directory

--- Savings web app/script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("Accumulated").addEventListener("click", function() {
-    window.location.href = "Accumulated.html"; // Change "anotherpage.html" to the URL of the page you want to navigate to
-});
-document.getElementById("SaveAmount").addEventListener("click", function() {
-    window.location.href = "SaveAmount.html"; // Change "anotherpage.html" to the URL of the page you want to navigate to
-});
-document.getElementById("GoodLiving").addEventListener("click", function() {
-    window.location.href = "GoodLiving.html"; // Change "anotherpage.html" to the URL of the page you want to navigate to
-});
